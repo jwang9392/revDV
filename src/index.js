ac3238e87988d04a91070ddea76eb23a5c2b2be1
jwang9392 <jwang9392@gmail.com>
2020-03-02 18:17:37 -0500

    First commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./styles/index.scss";
+
+window.addEventListener("DOMContentLoaded", () => {
+  const title = document.createElement("div");
+  title.classList.add("test-title");
+  title.innerHTML = `<h2>Hello World!</h2>`;
+  document.body.append(title);
+});
